dcd90bae29e770fe69fc299412b479cb33184d2d
[veerendra reddy] <[reddyveerendra535@gmail.com]>
2021-11-08 12:58:47 +0530

    first commit

---
+++ cv.py
@@ -0,0 +1,26 @@
+name=input("fullname:  ")
+company_name=input("company name:  ")
+date=input("date:  ")
+year=input("year of pursuing:  ")
+course=input("course:  ")
+branch=input("branch:  ")
+print(f"""
+from
+{name}.
+                                                                                            Date:{date}
+To,
+The Hr Manager ,
+{company_name} company,
+Dear Sir/Madam,
+                                    I am {name}, currently pursuing {year} year of {course} in {branch}.
+ As I am in the procedure of finding the right job, I identified your {company_name} company and find it 
+the right place as I will have the scope of being a part of the technical team, where I can execute my 
+programming knowledge, which is my area of interest.
+                                    I would like to be a part of this reputed firm, where I can utilize 
+my skills,knowledge & talent. This would be the right opportunity and I strongly believe that if given a
+chance I will prove myself and contribute to the growth & success of the organization.
+
+                       Thank you for your consideration.
+                                                                                        your sincerely,
+                                                                                        {name}
+""")
